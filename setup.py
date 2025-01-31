3366629c877956cccdc91583b0998d3ba986ddca
beth <bethk@uw.edu>
2025-01-31 13:54:40 -0800

    Update the setup file to include the v 14 txt file

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyIGRF",
-    version="0.3.3",
-    author="zzyztyy",
-    author_email="2375672032@qq.com",
-    description="IGRF-13 Model by Python",
+    version="0.3.4",
+    author="beth, forked from zzyztyy",
+    author_email="bethkirby@gmail.com",
+    description="IGRF-14 Model by Python",
     long_description=open("README.md").read(),
     license="MIT",
-    url="https://github.com/zzyztyy/pyIGRF",
+    url="https://github.com/Beth1708/pyIGRF",
     packages=find_packages(),
     install_requires=[
         "NumPy"
     ],
-    package_data={'': ['src/igrf13coeffs.txt']}
+    package_data={'': ['src/igrf13coeffs.txt', 'src/igrf14coeffs.txt']}
 )
